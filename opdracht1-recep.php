850398bc51e0d6ad7905eced2b12c2862e9d2fc4
DevvRecc <90837789+DevvRecc@users.noreply.github.com>
2022-06-02 12:46:27 +0200

    Add files via upload

---
+++ opdracht1-recep.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Auto{
+    public $merk;
+    public $kleur;
+    public $type;
+    public $uitvoering;
+    public $brandstof;
+}
+
+$nieuweAuto = new Auto();
+$nogEenAndereAuto = new Auto();
+
+?>
